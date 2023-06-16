925b6d67bf10eb492e345631d7f698e822cd1b7b
JotheeshKumar <80279461+jo73na@users.noreply.github.com>
2023-06-16 01:53:34 -0400

    Add files via upload
    animation and key strokes added

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-
-
-var bt=document.querySelectorAll(".drum").length;
+var bt = document.querySelectorAll(".drum").length;
 for (let index = 0; index < bt; index++) {
-    
-    document.querySelectorAll(".drum")[index].addEventListener("click",function() {
-    var ip=this.innerHTML;
-        switch(ip){
-            case "w":
-                handleFunc("tom-1");
-                break;
-            case "a":
-                handleFunc("tom-2");
-            break;
-    
-            case "s":
-               handleFunc("tom-3");
-            break;
-            case "d":
-                handleFunc("tom-4");
-            break;
-                            
-            case "j":
-                handleFunc("crash");
-            break;
-            case "k":
-                handleFunc("kick-bass");
-            break;
-                        
-            case "l":
-                handleFunc("snare");
-            break;
-    
-        }
+  document
+    .querySelectorAll(".drum")
+    [index].addEventListener("click", function () {
+      var keyorClick = this.innerHTML;
+      makeSound(keyorClick);
+      buttonAnimation(keyorClick);
     });
-   
 }
 
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
+function handleFunc(tom) {
+  var sound = new Audio("./sounds/" + tom + ".mp3");
+  sound.play();
+}
 
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      handleFunc("tom-1");
+      break;
+    case "a":
+      handleFunc("tom-2");
+      break;
 
+    case "s":
+      handleFunc("tom-3");
+      break;
+    case "d":
+      handleFunc("tom-4");
+      break;
 
+    case "j":
+      handleFunc("crash");
+      break;
+    case "k":
+      handleFunc("kick-bass");
+      break;
 
+    case "l":
+      handleFunc("snare");
+      break;
+  }
+}
 
-
-
-function handleFunc(tom){
-   new Audio("./sounds/"+tom+".mp3").play();
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
